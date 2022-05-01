4f08a257b5839635567bc9cd13983a6d58e67e7e
Camilo Terevinto <camiloterevinto@outlook.com>
2022-05-01 17:41:29 +0100

    Add a sample web application consuming the package

---
+++ Samples/WebApp/WebApp/CacheService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using TerevintoSoftware.AspNetCore.Authentication.ApiKeys.Abstractions;
+
+namespace WebApp
+{
+    public class CacheService : IApiKeysCacheService
+    {
+        private static readonly TimeSpan _cacheKeysTimeToLive = new(1, 0, 0);
+
+        private readonly IMemoryCache _memoryCache;
+        private readonly IClientsService _clientsService;
+
+        public CacheService(IMemoryCache memoryCache, IClientsService clientsService)
+        {
+            _memoryCache = memoryCache;
+            _clientsService = clientsService;
+        }
+
+        public async ValueTask<string?> GetOwnerIdFromApiKey(string apiKey)
+        {
+            if (!_memoryCache.TryGetValue<Dictionary<string, Guid>>("Authentication_ApiKeys", out var internalKeys))
+            {
+                internalKeys = await _clientsService.GetActiveClients();
+
+                _memoryCache.Set("Authentication_ApiKeys", internalKeys, _cacheKeysTimeToLive);
+            }
+
+            if (!internalKeys.TryGetValue(apiKey, out var clientId))
+            {
+                return null;
+            }
+
+            return clientId.ToString();
+        }
+
+        public async Task InvalidateApiKey(string apiKey)
+        {
+            if (_memoryCache.TryGetValue<Dictionary<string, Guid>>("Authentication_ApiKeys", out var internalKeys))
+            {
+                if (internalKeys.ContainsKey(apiKey))
+                {
+                    internalKeys.Remove(apiKey);
+                    _memoryCache.Set("Authentication_ApiKeys", internalKeys);
+                }
+            }
+
+            await _clientsService.InvalidateApiKey(apiKey);
+        }
+    }
+}
---
+++ Samples/WebApp/WebApp/Program.cs
@@ -0,0 +1,67 @@
+using TerevintoSoftware.AspNetCore.Authentication.ApiKeys;
+using TerevintoSoftware.AspNetCore.Authentication.ApiKeys.Abstractions;
+using WebApp;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddSimpleConsole(options => options.IncludeScopes = true);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(setup =>
+{
+    setup.AddApiKeySupport();
+});
+
+
+builder.Services
+    .AddDefaultApiKeyGenerator(new ApiKeyGenerationOptions
+    {
+        KeyPrefix = "CT-",
+        GenerateUrlSafeKeys = true,
+        LengthOfKey = 32
+    })
+    .AddDefaultClaimsPrincipalFactory()
+    .AddApiKeys()
+    .AddSingleton<IClientsService, InMemoryClientsService>()
+    .AddMemoryCache()
+    .AddSingleton<IApiKeysCacheService, CacheService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.RequireAuthorization();
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
